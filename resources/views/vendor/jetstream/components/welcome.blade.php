5c275a654bd9767a8b02d25f68672bb50ac24fac
Tu amigo Jean Piere <javierjeanpieres@gmail.com>
2022-12-13 23:52:50 -0300

    add - mejora

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -4,14 +4,14 @@
     </div>
 
     <div class="mt-8 text-2xl">
-        Welcome to your Jetstream application!
+        Bienvenido a MULTISERVICIOS LYR SAC!
     </div>
 
     <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+        MULTISERVICIOS es el nuevo servicio de entrega (courier) de MULTISERVICIOS que nació pensado como una solución a la coyuntura del COVID-19 y aún, luego del fin de la cuarentena, sigue dentro de nuestro servicios básicos.
+        Reafirmando nuestro compromiso y responsabilidad de mantener Lima conectada y a buen resguardo, MULTISERVICIOS busca atender las necesidades  de nuestros usuarios y acercarles la ciudad hasta la comodidad de sus hogares.
+        Con este nuevo servicio podrás trasladar objetos o paquetes pequeños de un lugar a otro en solo minutos y a precios convenientes.
+
     </div>
 </div>
 
